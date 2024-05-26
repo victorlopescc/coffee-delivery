9ec3581895bbb006d0751f42b181f4568e655382
victorlopescc <victorlopes.cc@gmail.com>
2024-05-26 13:17:32 -0300

    chore: Add cart functionality and actions for managing items

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,3 +1,20 @@
+import { z } from 'zod'
+
+const newOrder = z.object({
+  cep: z.number({ invalid_type_error: 'Informe o CEP' }),
+  street: z.string().min(1, 'Informe a rua'),
+  number: z.string().min(1, 'Informe o número'),
+  fullAddress: z.string(),
+  neighborhood: z.string().min(1, 'Informe o bairro'),
+  city: z.string().min(1, 'Informe a cidade'),
+  state: z.string().min(1, 'Informe a UF'),
+  paymentMethod: z.enum(['credit', 'debit', 'cash'], {
+    invalid_type_error: 'Informe um método de pagamento',
+  }),
+})
+
+export type OrderInfo = z.infer<typeof newOrder>
+
 export function Cart() {
   return <></>
 }
