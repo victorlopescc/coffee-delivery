9e215c0d63c1a94b2dcc2a9fe720c7389f41824d
victorlopescc <victorlopes.cc@gmail.com>
2024-05-27 14:36:33 -0300

    chore: Add Success page for displaying order confirmation details

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,89 @@
+import { useParams } from 'react-router-dom'
+import { useCart } from '../../hooks/useCart'
+import { useTheme } from 'styled-components'
+import {
+  InfoContainer,
+  OrderContainer,
+  OrderHeader,
+  OrderInfo,
+  SuccessContainer,
+} from './styles'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
+
 export function Success() {
-  return <></>
+  const { orders } = useCart()
+  const { orderId } = useParams()
+  const orderInfo = orders.find((order) => order.id === Number(orderId))
+  const paymentMethod = {
+    credit: 'Cartão de crédito',
+    debit: 'Cartão de débito',
+    cash: 'Dinheiro',
+  }
+  const theme = useTheme()
+
+  if (!orderInfo?.id) return null
+
+  return (
+    <SuccessContainer>
+      <OrderContainer>
+        <OrderHeader>
+          <h2>Uhu! Pedido confirmado</h2>
+          <span>Agora é só aguardar que logo o café chegará até você</span>
+        </OrderHeader>
+
+        <OrderInfo>
+          <InfoContainer>
+            <div>
+              <MapPin
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors.purple }}
+                size={32}
+              />
+
+              <div>
+                <span>
+                  Entrega em{' '}
+                  <strong>
+                    {orderInfo.street}, {orderInfo.number}
+                  </strong>
+                </span>
+
+                <span>
+                  {orderInfo.neighborhood} - {orderInfo.city}, {orderInfo.state}
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <Timer
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors.yellow }}
+                size={32}
+              />
+
+              <div>
+                <span>Previsão de entrega</span>
+                <strong>20 min - 30 min</strong>
+              </div>
+            </div>
+
+            <div>
+              <CurrencyDollar
+                color={theme.colors.white}
+                style={{ backgroundColor: theme.colors['yellow-dark'] }}
+                size={32}
+              />
+
+              <div>
+                <span>Pagamento na entrega</span>
+                <strong>{paymentMethod[orderInfo.paymentMethod]}</strong>
+              </div>
+            </div>
+          </InfoContainer>
+        </OrderInfo>
+      </OrderContainer>
+
+      <img src="/images/delivery.svg" alt="Pedido concluído" />
+    </SuccessContainer>
+  )
 }
